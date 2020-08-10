499c2e5219ecb7d344937e161f15e08a7734d820
khanu <usman.khan@pega.com>
2020-08-10 10:53:09 -0400

    Updates

---
+++ precache-manifest.54ed1e23f0c989dd27f1506bef44eb98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6acfaaee15626bc7982ca5e62166e525",
+    "url": "/react-employee-directory/index.html"
+  },
+  {
+    "revision": "237c4b459e3551ffc246",
+    "url": "/react-employee-directory/static/css/main.69b8bd75.chunk.css"
+  },
+  {
+    "revision": "28c866d452f4cd841475",
+    "url": "/react-employee-directory/static/js/2.fb3aa9ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-employee-directory/static/js/2.fb3aa9ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "237c4b459e3551ffc246",
+    "url": "/react-employee-directory/static/js/main.5d2e99c6.chunk.js"
+  },
+  {
+    "revision": "e82547d4095795d10a4a",
+    "url": "/react-employee-directory/static/js/runtime-main.93471c7f.js"
+  }
+]);
